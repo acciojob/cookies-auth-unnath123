6a9c6721b0697519b6b713fb4f2a3706f566867f
unnath123 <64188873+unnath123@users.noreply.github.com>
2024-02-07 14:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+if(sessionStorage.getItem("token")){
+    // window.location.href = "./secondpage.html"
+}
+else{
+    document.getElementById("btn").addEventListener("click", function(){
+        console.log(username.value)
+        console.log(password.value);
+        // window.location.href = "./secondpage.html"
+        sessionStorage.setItem("token", username.value);
+    })
+}
+
+
